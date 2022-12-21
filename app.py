fd103f3eaf0dd82f78125b3ae02f405b8827d21f
Nico Escovilla <35411308+nicoEXE@users.noreply.github.com>
2022-12-21 17:04:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text",  server_port=10000, server_name="0.0.0.0")
 demo.launch()
